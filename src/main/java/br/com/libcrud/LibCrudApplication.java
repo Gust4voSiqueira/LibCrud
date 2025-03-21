60d78a09d5698c8bacd76aee3bba84ec0886ff38
gustavo.siqueira <gustavo.siqueira@cwi.com.br>
2025-03-21 15:57:02 -0300

    Adiciona swagger

--- src/main/java/br/com/libcrud/LibCrudApplication.java
+++ src/main/java/br/com/libcrud/LibCrudApplication.java
@@ -1,9 +1,18 @@
 package br.com.libcrud;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "LibCrud",
+				description = "Api com Crud simples para cursos",
+				version = "1.0"
+		)
+)
 public class LibCrudApplication {
 
 	public static void main(String[] args) {
